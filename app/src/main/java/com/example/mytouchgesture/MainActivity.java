7da7130e61176b4aa9b0f3b4d2c8bf2d2b1c4681
kimura121212 <ykimuranoi@gmail.com>
2020-04-10 01:23:45 +0900

    first commit

---
+++ app/src/main/java/com/example/mytouchgesture/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.mytouchgesture;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
+import androidx.core.view.MotionEventCompat;
+import androidx.core.view.VelocityTrackerCompat;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final String DEBUG_TAG = "MultiTouchGesture";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        // Be sure to call the superclass implementation
+        int index = event.getActionIndex();
+        int action = event.getActionMasked();
+        int pointerId = event.getPointerId(index);
+
+        int xPos = -1;
+        int yPos = -1;
+
+        Log.d(DEBUG_TAG,"The action is " + actionToString(action));
+
+        if (event.getPointerCount() > 1) {
+            // The coordinates of the current screen contact, relative to
+            // the responding View or Activity.
+            xPos = (int)MotionEventCompat.getX(event, index);
+            yPos = (int)MotionEventCompat.getY(event, index);
+            Log.d(DEBUG_TAG,"Multitouch event index:" + index + " x:" + xPos + " y:" + yPos);
+
+        } else {
+            // Single touch event
+            xPos = (int)MotionEventCompat.getX(event, index);
+            yPos = (int)MotionEventCompat.getY(event, index);
+            Log.d(DEBUG_TAG,"Single touch event index:" + index + " x:" + xPos + " y:" + yPos);
+        }
+
+        return true;
+    }
+
+    // Given an action int, returns a string description
+    public static String actionToString(int action) {
+        switch (action) {
+
+            case MotionEvent.ACTION_DOWN: return "Down";
+            case MotionEvent.ACTION_MOVE: return "Move";
+            case MotionEvent.ACTION_POINTER_DOWN: return "Pointer Down";
+            case MotionEvent.ACTION_UP: return "Up";
+            case MotionEvent.ACTION_POINTER_UP: return "Pointer Up";
+            case MotionEvent.ACTION_OUTSIDE: return "Outside";
+            case MotionEvent.ACTION_CANCEL: return "Cancel";
+        }
+        return "";
+    }
+}
---
+++ app/src/main/java/com/example/mytouchgesture/MyCustomView.java
@@ -0,0 +1,70 @@
+package com.example.mytouchgesture;
+
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+
+import androidx.core.view.MotionEventCompat;
+
+
+public class MyCustomView extends View {
+
+    private RectF mBounds;
+    private Paint mPaint;
+
+    private ScaleGestureDetector mScaleDetector;
+    private float mScaleFactor = 1.f;
+
+    public MyCustomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mBounds = new RectF(0,0, 300, 400);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(Color.BLUE);
+
+        mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.save();
+        canvas.scale(mScaleFactor, mScaleFactor);
+
+        canvas.drawRect(
+                mBounds,
+                mPaint
+        );
+
+        canvas.restore();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        // Let the ScaleGestureDetector inspect all events.
+        mScaleDetector.onTouchEvent(ev);
+        return true;
+    }
+
+    private class ScaleListener
+            extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            mScaleFactor *= detector.getScaleFactor();
+
+            // Don't let the object get too small or too large.
+            mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 5.0f));
+
+            invalidate();
+            return true;
+        }
+    }
+}
